9dd9cf74644f384454febc6d37bc6f41f97c0451
Nils Preusker <nils.preusker@movingimage24.de>
2014-03-28 17:21:40 +0100

    Securing web app with keycloak and showcasing how to access user info (username and roles)

--- src/main/java/de/nilspreusker/devoxx13/todo/boundary/TodoResource.java
+++ src/main/java/de/nilspreusker/devoxx13/todo/boundary/TodoResource.java
@@ -4,9 +4,11 @@ import de.nilspreusker.devoxx13.todo.jaxrs.PATCH;
 import de.nilspreusker.devoxx13.todo.model.TodoItem;
 import de.nilspreusker.devoxx13.todo.service.TodoItemService;
 
+import javax.annotation.security.RolesAllowed;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.persistence.EntityNotFoundException;
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
@@ -15,6 +17,9 @@ import javax.ws.rs.core.UriInfo;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
+import org.jboss.ejb3.annotation.SecurityDomain;
+import org.keycloak.KeycloakSecurityContext;
+
 
 /**
  * @author Nils Preusker - n.preusker@gmail.com - http://www.nilspreusker.de
@@ -22,13 +27,17 @@ import java.util.List;
 
 @Path("todo")
 @Stateless
+@SecurityDomain("keycloak")
 public class TodoResource {
 
     @Inject
     private TodoItemService service;
 
+    @Context private HttpServletRequest servletRequest;
+
     // create
     @POST
+    @RolesAllowed("user")
     @Consumes(MediaType.APPLICATION_JSON)
     public Response createTodo(@Context UriInfo uriInfo, TodoItem todo) throws URISyntaxException {
         service.saveTodoItem(todo);
@@ -38,7 +47,20 @@ public class TodoResource {
     // get all
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    @RolesAllowed("user")
     public Response getAll() {
+
+        // Showcase how to access user info provided by keycloak
+        KeycloakSecurityContext ctx = (KeycloakSecurityContext) servletRequest.getAttribute(KeycloakSecurityContext.class.getName());
+        System.out.println("----");
+        System.out.println("User name: " + ctx.getToken().getPreferredUsername());
+        System.out.println("----");
+        System.out.println("User is user: " + servletRequest.isUserInRole("user"));
+        System.out.println("User is admin: " + servletRequest.isUserInRole("admin"));
+        System.out.println("----");
+        System.out.println("User name: " + servletRequest.getRemoteUser());
+        System.out.println("----");
+
         List<TodoItem> items = service.findAllTodoItems();
         return Response.ok(items).build();
     }
@@ -46,6 +68,7 @@ public class TodoResource {
     // get by id
     @GET
     @Path("/{id}")
+    @RolesAllowed("user")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getById(@PathParam("id") long id) {
         try {
@@ -59,6 +82,7 @@ public class TodoResource {
     // update
     @PATCH
     @Path("/{id}")
+    @RolesAllowed("user")
     public Response update(@PathParam("id") long id, TodoItem item) {
         try {
             service.updateTodoItem(item);
@@ -71,6 +95,7 @@ public class TodoResource {
     // delete
     @DELETE
     @Path("/{id}")
+    @RolesAllowed("user")
     public Response delete(@PathParam("id") long id) {
         try {
             service.deleteTodoItem(id);
